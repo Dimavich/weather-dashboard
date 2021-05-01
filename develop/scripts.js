f5befc6a623c2385f6d611109434dfa1c635b3c0
Dmitriy <dmitriy.babich0@gmail.com>
2021-04-30 19:08:37 -0700

    the current tab works except the uv index

--- develop/scripts.js
+++ develop/scripts.js
@@ -5,13 +5,14 @@ var todayTemp = document.getElementById('today-temp');
 var humidity = document.getElementById('humidity');
 var wind = document.getElementById('wind');
 var uv = document.getElementById('uv');
+var mainIcon = document.getElementById('mainIcon');
 var searchBtn = document.getElementById('search-btn');
 var searchVal = document.getElementById('search-bar');
 // API key
 var apiKey = 'deaeafb532c77ef1da766b428ecb34ef';
 
 
-// calling the weather api
+// calling the current weather api
 function search (event){
   event.preventDefault();
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchVal.value}&units=imperial&appid=${apiKey}`, {
@@ -22,7 +23,15 @@ function search (event){
     })
     .then(function (data) {
       console.log(data);
+      // changes the data for current weather
+      city.textContent = data.name;
+      todayTemp.textContent = 'Temp:  ' + data.main.temp + ' F°';
+      humidity.textContent = 'Humidity:   ' + data.main.humidity + ' %';
+      wind.textContent = 'Wind:   ' + data.wind.speed + ' MPH';
+      var iconCodeMain = data.weather[0].icon;
+      mainIcon.src=`./images/${iconCodeMain}.png`;
     });
+  
 };
 
 
